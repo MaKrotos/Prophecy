2f09b89f5d27f29d20b13417878238d5159fe16d
Lapshin <lapshin@slotlogic.eu>
2025-10-11 21:44:25 +0300

    Refactoring BottomNav

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,7 +3,33 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    }
+  },
   server: {
-    allowedHosts: ['rhwfn-85-198-105-19.a.free.pinggy.link']
+    proxy: {
+      '/api': {
+        target: 'http://backend-prophecy:8080',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy, options) => {
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            // Безопасная установка заголовков
+            const remoteAddress = req.socket.remoteAddress || '';
+            const forwardedFor = req.headers['x-forwarded-for'] || remoteAddress || '';
+            const protocol = req.protocol || (req.connection?.encrypted ? 'https' : 'http') || 'http';
+            
+            proxyReq.setHeader('X-Real-IP', remoteAddress);
+            proxyReq.setHeader('X-Forwarded-For', forwardedFor);
+            proxyReq.setHeader('X-Forwarded-Proto', protocol);
+          });
+        },
+        proxyTimeout: 30000,
+        timeout: 30000,
+      }
+    }
   }
 });
