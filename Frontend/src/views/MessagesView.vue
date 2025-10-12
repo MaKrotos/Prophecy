7ba4e41b7688a9d1c2c6ad2020aa7e8890e968a7
Lapshin <lapshin@slotlogic.eu>
2025-10-12 16:27:33 +0300

    delete duplicates from localization

--- Frontend/src/views/MessagesView.vue
+++ Frontend/src/views/MessagesView.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="page">
-    <h2>Сообщения</h2>
-    <p>Страница сообщений</p>
+    <h2>{{ t('messages_view.title') }}</h2>
+    <p>{{ t('messages_view.description') }}</p>
   </div>
 </template>
+
+<script setup>
+import { useLocalization } from '/src/locales/index.js'
+
+const { t } = useLocalization()
+</script>
